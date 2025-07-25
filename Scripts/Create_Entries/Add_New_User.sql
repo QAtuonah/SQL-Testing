7d5dd36385395538f0b438de23d0f3d971238cd3
Christopher Atuonah <chr.dubem@gmail.com>
2025-07-25 19:11:30 -0300

    Create Add_New_User.sql

---
+++ Scripts/Create_Entries/Add_New_User.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (
+  userId, password, email, firstName, lastName, address1,
+  zipcode, city, state, country, phone
+) VALUES (
+  1, 'Test@123', 'john.doe@example.com', 'John', 'Doe', '123 Street',
+  '90210', 'LA', 'CA', 'USA', '555-1234'
+);
